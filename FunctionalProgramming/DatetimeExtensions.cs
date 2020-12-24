0164762f3d6c3dd6dacaa8697c5436689b07820a
Yael Chavez <you400@gmail.com>
2020-12-24 00:53:35 -0600

    ExtensionMethods implementation

---
+++ FunctionalProgramming/DatetimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+
+    /// <summary>
+    /// Problem: we have to pass datetimes to another system wich needs the dates as shown below 
+    /// 20180205154537 <- the format is (yyyyMMddhhmmss)
+    /// 9501301692456 <- same but the year is on two digits
+    /// Solve: we are goning to make a simple implementation of Datetime extensions as below
+    /// </summary>
+    public static class DatetimeExtensions
+    {
+        public static string ToDevideFormat(this DateTime dt) =>
+            dt.Year >= 2000 ? dt.ToString("yyyyMMddhhmmss") : dt.ToString("yyyyMMddhhmmss").Substring(2);
+
+    }
+}
--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using ExtensionMethods;
+using System;
 
 namespace Extensions
 {
     class Program
     {
+        /// <summary>
+        /// implementation of a Extension method from datetime
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime dt1 = new DateTime(2020, 12, 12, 01, 02, 03);
+            var result = dt1.ToDevideFormat();
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+            DateTime dt2 = new DateTime(1999, 12, 12, 01, 02, 03);
+            var result2 = dt2.ToDevideFormat();
+            Console.WriteLine(result2);
+
+            Console.ReadLine();
         }
     }
 }
