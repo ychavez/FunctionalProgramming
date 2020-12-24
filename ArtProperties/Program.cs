04b988d58e178d583ffb3588cd2cd05e829294cd
Yael Chavez <you400@gmail.com>
2020-12-24 01:02:34 -0600

    Autoproperties

---
+++ ArtProperties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Autoproperties
+{
+
+    public class Character {
+
+        public int Armor { get; } = 100;
+
+        public Character()
+        {
+            Armor = 90;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character c = new Character();
+            Console.WriteLine(c.Armor);
+            //look how constructor has the priority
+            Console.ReadLine();
+        }
+    }
+}
