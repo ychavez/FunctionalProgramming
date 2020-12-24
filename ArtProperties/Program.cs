b4b7322727ca4879be3fdba5e309fe9d9963b36f
Yael Chavez <you400@gmail.com>
2020-12-24 01:10:23 -0600

    Expression body

--- ArtProperties/Program.cs
+++ ArtProperties/Program.cs
@@ -3,9 +3,21 @@
 namespace Autoproperties
 {
 
-    public class Character {
+    public class Character
+    {
 
         public int Armor { get; } = 100;
+
+        ///  public int Armor  => 100; still being inmutable
+        ///  public int Armor  = 100; !!! be carefull with that!!!
+
+        public int Wear { get; private set; } = 15;
+        public int Healt { get; private set; } = 100;
+
+        ///ExpressiBonbody function
+        public int Defense => Wear >= Armor ? 0 : Armor - Wear;
+
+        public void Damage(int damage) => Healt -= damage - Defense;
 
         public Character()
         {
