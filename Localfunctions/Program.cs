39152159d9e3231b6cd8d5d9b22f5a0cfb277799
Yael Chavez <you400@gmail.com>
2020-12-25 17:37:41 -0600

    Local Functions

---
+++ Localfunctions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LocalFunctions
+{
+    public class Customer
+    {
+        public string GetFullInfo(string fullStr)
+        {
+            return GetFullName(fullStr).firstName + GetFullName(fullStr).lastName;
+            (string firstName, string lastName) GetFullName(string str)
+            {
+                return (str.Split()[0], str.Split()[1]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
