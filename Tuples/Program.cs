10cf58508eed33d435a0f38e9241bc2fe4f335fd
Yael Chavez <you400@gmail.com>
2020-12-25 17:26:07 -0600

    Tuples

---
+++ Tuples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Tuples
+{
+    public class RocketLauncher {
+        public (int mainSystemError, int subSystemError) LaunchRocket() {
+            Launch();
+            return (GetMainSystemState(), GetSubsystemState());
+        }
+
+        private int GetSubsystemState()
+        {
+            return 0;
+        }
+
+        private int GetMainSystemState()
+        {
+            return 0;
+        }
+
+        private void Launch()
+        {
+            
+        }
+    }
+
+    public class Point {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Deconstruction(out int x, out int y) {
+            x = X;
+            y = Y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RocketLauncher launcher = new RocketLauncher();
+            var results = launcher.LaunchRocket();
+            Console.WriteLine(results.mainSystemError);
+            Console.WriteLine(results.subSystemError);
+            (int first1, int second1) = launcher.LaunchRocket();
+            (var first2, var second2) = launcher.LaunchRocket();
+            var (first3, second3) = launcher.LaunchRocket();
+        }
+    }
+}
