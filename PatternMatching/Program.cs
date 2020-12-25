02dabd7b3d2c8606d26cbd02f7901ccc2574b584
Yael Chavez <you400@gmail.com>
2020-12-25 17:11:42 -0600

    PatternMatching

---
+++ PatternMatching/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PatternMatching
+{
+    public class Circle
+    {
+        public int Radius { get; set; }
+    }
+    public class Triangle
+    {
+        public int SideAB { get; set; }
+        public int SideBC { get; set; }
+        public int SideAC { get; set; }
+    }
+    class Program
+    {
+        static void SwitchCasePatternMatching(object o)
+        {
+            switch (o)
+            {
+                case Triangle t:
+                    {
+                        Console.WriteLine($"AB:{t.SideAB}");
+                    }
+                    break;
+                case Circle c:
+                    {
+                        Console.WriteLine($"Radius:{c.Radius}");
+                    }
+                    break;
+            }
+        }
+        static void StraightPatternMatching(object o)
+        {
+            if (o is int x || (o is string s && int.TryParse(s, out x)))
+                Console.WriteLine($"X parsed with matching:{x}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
