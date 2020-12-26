4e44616c1dff054fee94de09723d6d0c95738184
Yael Chavez <you400@gmail.com>
2020-12-25 22:23:46 -0600

    ShalloInwmutability

---
+++ Inmutability/ShallowInmutability.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inmutability.ShalloInwmutability
+{
+
+    public class Account
+    {
+        public string Name { get; }
+        //list CouldBe inmutable
+        public IReadOnlyCollection<string> EMails { get; }
+
+        public Account(string name, IReadOnlyCollection<string> emails)
+        {
+            Name = name;
+            EMails = emails;
+        }
+
+    }
+    public class AccountClient
+    {
+        public static void Run()
+        {
+            var emails = new List<string>();
+            var acc = new Account("NoName", emails);
+        }
+    }
+
+}
---
+++ Inmutability/Shallowmutability.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inmutability.Shallowmutability
+{
+
+    public class Account
+    {
+        public string Name { get; }
+       //list CouldBe inmutable
+        public List<string> EMails { get; }
+
+        public Account(string name, List<string> emails)
+        {
+            Name = name;
+            EMails = emails;
+        }
+
+    }
+    public class AccountClient
+    {
+        public static void Run()
+        {
+            var emails = new List<string>();
+            var acc = new Account("NoName", emails);
+            emails.Add("abc@gmail.com"); // still being mutable
+        }
+    }
+}
